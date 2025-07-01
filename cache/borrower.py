5ac8675bc65bda1b270b2e31b4ad9d6f164faac9
심현진 <hyunjin.dev143@gmail.com>
2025-07-01 10:22:45 +0900

    fix: redis-cli 접속 시 유의사항

--- cache/borrower.py
+++ cache/borrower.py
@@ -3,6 +3,9 @@ from . import r
 def test():
     return "redis connect ok"
 
+# 주의: 레디스 cli 접속 시  docker exec -it redis redis-cli --raw 로 접속해야 한글 인코딩이 깨지지 않습니다!!
+# 키를 조회했을 때 이상하게 뜬다면 위와 같이 접속해 주세요!!
+
 # 도서 제목을 대출자에 맞춰 Redis에 저장
 def cache_borrowed_book(borrower: str, title: str):
     key = f"borrower:{borrower}:books"
